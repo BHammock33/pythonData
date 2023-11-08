3656948775e75d294e6441c87a3b2f4d72de2b37
BHammock33 <BennettHammock@gmail.com>
2023-11-08 17:24:21 -0500

    add in CSV

--- functions.py
+++ functions.py
@@ -8,3 +8,47 @@ for rating in ratings:
     else:
         content_ratings[rating] = 1
 print(content_ratings)            
+
+def square(a_number):
+    squared_number = a_number * a_number
+    return squared_number
+print(square(a_number=6))
+print(square(a_number=4))
+print(square(a_number=9))
+print(square(10))
+
+opened_file = open('AppleStore.csv')
+from csv import reader
+read_file = reader(opened_file)
+apps_data = list(read_file)
+
+def extract(index):
+    column = []
+    for row in apps_data[1:]:
+        value = row[index]
+        column.append(value)
+    return column
+genres = extract(11)
+
+def freq_table(column):
+    freq_list = {}
+    for value in column:
+        if value in freq_list:
+            freq_list[value] += 1
+        else:
+            freq_list[value] = 1
+    return freq_list
+genres_ft = freq_table(genres)
+print(genres_ft)        
+
+def freq_table_2(index):
+    ratings_table2 = {}
+    for row in apps_data[1:]:
+        value = row[index]
+        if value in ratings_table2:
+            ratings_table2[value] += 1
+        else:
+            ratings_table2[value] =1
+    return ratings_table2
+ratings_ft = freq_table_2(7)
+print(ratings_ft)
